e838d4ba6072b780f83e8f2a597daad4a89f7a67
samkele2 <samkelenjiva@gmail.com>
2025-07-17 20:19:29 +0300

    Deep

---
+++ Deep/deep.py
@@ -0,0 +1,6 @@
+mrt = input("What's the Answer to the Great Question of Life, the Universe, and Everything?"  )
+
+if mrt == "42" or mrt == "forty two" or mrt == "forty-two":
+    print("Yes")
+else:
+    print("No")
--- Tip/tip.py
+++ Tip/tip.py
@@ -1,12 +1,12 @@
 def main():
     dollars = dollars_to_float(input("How much was the meal? "))
     percent = percent_to_float(input("What percentage would you like to tip? "))
-    tip = dollars * percent
+    tip = dollars*percent
     print(f"Leave ${tip:.2f}")
 
 def dollars_to_float(d):
     d_fl = d.strip("$")
-    return d_fl
+    return float(d_fl)
 
 def percent_to_float(p):
     p_fl = p.strip("%")
