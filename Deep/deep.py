94aa3fd0aafae17d4e5556d8f318ea13f57f5e9b
samkele2 <samkelenjiva@gmail.com>
2025-07-23 20:11:47 +0300

    Update deep.py

--- Deep/deep.py
+++ Deep/deep.py
@@ -1,6 +1,8 @@
 mrt = input("What's the Answer to the Great Question of Life, the Universe, and Everything?"  )
+mrt_l = mrt.lower().strip(" ")
 
-
-def deep(mrt):
-   mrt_l = mrt.lower()
+if mrt_l == "forty two" or mrt_l == "forty-two" or mrt_l == "42":
+   print("Yes")
+else:
+   print("No")
     
